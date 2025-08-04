4824076925ede03e89c67197a6eb66f5ec2e84aa
Prathamesh <prathameshteli727@gmail.com>
2025-08-05 03:02:37 +0530

    all routes of backed working

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ connectTomongo();
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
@@ -18,22 +18,3 @@ app.use("/api/notes", require("./routes/notes.js"));
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
-
-// const connectTomongo = require("./db.js");
-// connectTomongo();
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.use('/api/auth',require('./routes/auth.js'));
-// app.use('/api/notes',require('./routes/notes.js'));
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port http://localhost:${port}`)
-// })
